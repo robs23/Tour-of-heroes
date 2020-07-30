b06e0171a4c0353d1cc66def38e0cecc2d2e84eb
Robert Roszak <robert.roszak@outlook.com>
2020-07-30 20:23:52 +0200

    With reuseable hero-detail subcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      HeroesComponent,
+      HeroDetailComponent
+   ],
+   imports: [
+      BrowserModule,
+      FormsModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,11 +10,10 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
 
   selectedHero: Hero;
+  heroes = HEROES;
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
   }
-  
-  heroes = HEROES;
   constructor() { }
 
   ngOnInit(): void {
