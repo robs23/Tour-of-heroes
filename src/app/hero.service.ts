e41a548391ce807a05a2aebe586af0ce851ab33c
Robert Roszak <robert.roszak@outlook.com>
2020-07-31 15:23:42 +0200

    With routing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,4 +16,9 @@ getHeroes(): Observable<Hero[]>{
   return of(HEROES);
 }
 
+getHero(id: number): Observable<Hero>{
+  this.messageService.add(`HeroService: fetched hero id=${id}`);
+  return of(HEROES.find(hero=>hero.id ===id));
+}
+
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,12 +10,7 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
 
-  selectedHero: Hero;
   heroes: Hero[];
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-    this.messageService.add('HeroesComponent: Selected hero id=${hero.id}');
-  }
   constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
